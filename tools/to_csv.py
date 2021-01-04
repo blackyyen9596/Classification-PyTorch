52605637b837963a19ac6af8cdea13742f5d8e3d
Chen-Yen Chang <66304059+BlackyYen@users.noreply.github.com>
2021-01-04 13:19:11 +0800

    Delete

--- tools/to_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def to_csv(id_read_path, save_path, y_pred):
-    my_submission = pd.DataFrame({
-        'id': pd.read_csv(id_read_path).id,
-        'character': y_pred[:]
-    })
-    my_submission.to_csv(save_path, index=False)
